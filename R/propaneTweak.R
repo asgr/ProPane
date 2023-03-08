62d6ae7b643cbd9100cd968b6a43c0ef328661ab
Aaron Robotham <aaron.robotham@uwa.edu.au>
2023-03-08 13:25:21 +0800

    Now tweaking works on WCS directly.
    Added tweaking related vignette.

--- R/propaneTweak.R
+++ R/propaneTweak.R
@@ -1,5 +1,5 @@
 propaneTweak = function(image_ref, image_pre_fix, delta_max=c(3,0), quan_cut=0.99, Nmeta=3,
-                      cores=4, shift_int=TRUE, return_image=TRUE, direction='backward',
+                      WCS_match=TRUE, cores=4, shift_int=TRUE, return_image=TRUE, direction='backward',
                       final_centre=TRUE, verbose=TRUE){
 
   if(!requireNamespace("imager", quietly = TRUE)){
@@ -8,14 +8,37 @@ propaneTweak = function(image_ref, image_pre_fix, delta_max=c(3,0), quan_cut=0.9
 
   timestart = proc.time()[3]
 
-  if(inherits(image_ref, 'Rfits_image')){
-    image_ref_orig = image_ref
-    image_ref = image_ref$imDat
+  if(shift_int){
+    WCS_match = TRUE
   }
 
+  if(WCS_match){
+    if(all(dim(image_ref) != dim(image_pre_fix))){
+      stop('If WCS_match or shift_int is TRUE then dimensions of image_ref and image_pre_fix must match!')
+    }
+  }
+
+  if(WCS_match==FALSE){
+    if(inherits(image_ref, 'Rfits_image') == FALSE){
+      stop('image_ref must be class Rfits_image if WCS_match=FALSE')
+    }
+    if(inherits(image_pre_fix, 'Rfits_image') == FALSE){
+      stop('image_pre_fix must be class Rfits_image if WCS_match=FALSE')
+    }
+  }
+
+  if(inherits(image_ref, 'Rfits_image')){
+    if(WCS_match){
+      image_ref = image_ref$imDat
+    }
+  }
+
+  image_pre_fix_orig = image_pre_fix
+
   if(inherits(image_pre_fix, 'Rfits_image')){
-    image_pre_fix_orig = image_pre_fix
-    image_pre_fix = image_pre_fix$imDat
+    if(WCS_match){
+      image_pre_fix = image_pre_fix$imDat
+    }
   }
 
   dim_orig = dim(image_ref)
@@ -24,21 +47,37 @@ propaneTweak = function(image_ref, image_pre_fix, delta_max=c(3,0), quan_cut=0.9
     message("Trimming comparison region and auto scaling")
   }
 
-  trim_lim = which((!is.na(image_ref)) & (!is.na(image_pre_fix)), arr.ind = TRUE)
-  x_lo = min(trim_lim[,1])
-  x_hi = max(trim_lim[,1])
-  y_lo = min(trim_lim[,2])
-  y_hi = max(trim_lim[,2])
+  if(WCS_match){
+    trim_lim = which((!is.na(image_ref)) & (!is.na(image_pre_fix)), arr.ind = TRUE)
+    x_lo = min(trim_lim[,1])
+    x_hi = max(trim_lim[,1])
+    y_lo = min(trim_lim[,2])
+    y_hi = max(trim_lim[,2])
 
-  image_ref = image_ref[x_lo:x_hi, y_lo:y_hi]
-  image_ref = image_ref - median(image_ref, na.rm=TRUE)
-  image_ref = image_ref / quantile(image_ref, quan_cut, na.rm=TRUE)
-  image_ref[image_ref < 1] =NA
+    image_ref = image_ref[x_lo:x_hi, y_lo:y_hi]
+    image_pre_fix = image_pre_fix[x_lo:x_hi, y_lo:y_hi]
+  }
 
-  image_pre_fix_temp = image_pre_fix[x_lo:x_hi, y_lo:y_hi]
-  image_pre_fix = image_pre_fix_temp - median(image_pre_fix_temp, na.rm=TRUE)
-  image_pre_fix = image_pre_fix / quantile(image_pre_fix, quan_cut, na.rm=TRUE)
-  image_pre_fix[image_pre_fix < 1] = NA
+  if(inherits(image_ref, 'Rfits_image')){
+    image_ref$imDat = image_ref$imDat - median(image_ref$imDat, na.rm=TRUE)
+    image_ref$imDat = image_ref$imDat / quantile(image_ref$imDat, quan_cut, na.rm=TRUE)
+    image_ref$imDat[image_ref$imDat < 1] = NA
+  }else{
+    image_ref = image_ref - median(image_ref, na.rm=TRUE)
+    image_ref = image_ref / quantile(image_ref, quan_cut, na.rm=TRUE)
+    image_ref[image_ref < 1] = NA
+  }
+
+  if(inherits(image_pre_fix, 'Rfits_image')){
+    image_pre_fix$imDat = image_pre_fix$imDat - median(image_pre_fix$imDat, na.rm=TRUE)
+    image_pre_fix$imDat = image_pre_fix$imDat / quantile(image_pre_fix$imDat, quan_cut, na.rm=TRUE)
+    image_pre_fix$imDat[image_pre_fix$imDat < 1] = NA
+  }else{
+    image_pre_fix = image_pre_fix - median(image_pre_fix, na.rm=TRUE)
+    image_pre_fix = image_pre_fix / quantile(image_pre_fix, quan_cut, na.rm=TRUE)
+    image_pre_fix[image_pre_fix < 1] = NA
+  }
+
 
   scale = 1 # not really used anymore
   i = NULL #to avoid warnings
@@ -50,6 +89,7 @@ propaneTweak = function(image_ref, image_pre_fix, delta_max=c(3,0), quan_cut=0.9
   }
 
   if(shift_int){
+
     registerDoParallel(cores=cores)
 
     Ndelta = length(-delta_max[1]:delta_max[1])
@@ -121,6 +161,7 @@ propaneTweak = function(image_ref, image_pre_fix, delta_max=c(3,0), quan_cut=0.9
     optim_out$par_weight = c(par_weight_x, par_weight_y)
 
   }else{
+
     if(delta_max[2] == 0){
       par = c(0,0)
       lower = c(-delta_max[1], -delta_max[1])
@@ -130,7 +171,8 @@ propaneTweak = function(image_ref, image_pre_fix, delta_max=c(3,0), quan_cut=0.9
       lower = c(-delta_max[1], -delta_max[1], -delta_max[2])
       upper = c(delta_max[1], delta_max[1], delta_max[2])
     }
-    optim_out = optim(par =par,
+
+    optim_out = optim(par = par,
                       fn = .cost_fn,
                       method = "L-BFGS-B",
                       lower = lower,
@@ -140,7 +182,8 @@ propaneTweak = function(image_ref, image_pre_fix, delta_max=c(3,0), quan_cut=0.9
                       scale = scale,
                       direction = direction,
                       pix_cost_use = NULL,
-                      shift_int = FALSE
+                      shift_int = FALSE,
+                      WCS_match = WCS_match
     )
   }
 
@@ -151,9 +194,9 @@ propaneTweak = function(image_ref, image_pre_fix, delta_max=c(3,0), quan_cut=0.9
 
     if(inherits(image_pre_fix_orig, 'Rfits_image')){
       if(length(optim_out$par) == 2){
-        image_post_fix = propaneWCSmod(image_pre_fix_orig, optim_out$par[1], optim_out$par[2])
+        image_post_fix = propaneWCSmod(image_pre_fix_orig, delta_x=optim_out$par[1], delta_y=optim_out$par[2])
       }else{
-        image_post_fix = propaneWCSmod(image_pre_fix_orig, optim_out$par[1], optim_out$par[2], optim_out$par[3])
+        image_post_fix = propaneWCSmod(image_pre_fix_orig, delta_x=optim_out$par[1], delta_y=optim_out$par[2], delta_rot=optim_out$par[3])
       }
     }else{
       image_post_fix_temp = .cost_fn(par = optim_out$par,
@@ -238,51 +281,6 @@ propaneTran = function(image, delta_x = 0, delta_y = 0, delta_rot = 0, xcen_rot 
   }
 }
 
-.map.tran = function(x=0, y=0, delta_x=0, delta_y=0, delta_rot=0, xcen_rot=0, ycen_rot=0){
-  if(delta_rot != 0){
-    x_mod = (x - xcen_rot)*cos(delta_rot*pi/180) + (y - ycen_rot)*sin(delta_rot*pi/180)
-    y_mod = -(x - xcen_rot)*sin(delta_rot*pi/180) + (y - ycen_rot)*cos(delta_rot*pi/180)
-    x_mod = x_mod + xcen_rot + delta_x
-    y_mod = y_mod + ycen_rot + delta_y
-  }else{
-    x_mod = x + delta_x
-    y_mod = y + delta_y
-  }
-  list(x = x_mod, y = y_mod)
-}
-
-.cost_fn = function(par, image_ref, image_pre_fix, scale=1, direction='backward', pix_cost_use=NULL, shift_int = TRUE, return='cost'){
-  if(shift_int){
-    cost = .mat_diff_sum(image_ref, image_pre_fix, scale, par[1], par[2])
-    message(par[1],' ',par[2],' ',cost)
-    return(cost)
-  }else{
-    if(length(par) == 2){
-      image_post_fix = propaneTran(image_pre_fix, delta_x=par[1], delta_y=par[2], padNA=FALSE, shift_int=FALSE)
-    }else if (length(par)==3){
-      image_post_fix = propaneTran(image_pre_fix, delta_x=par[1], delta_y=par[2], delta_rot=par[3], padNA=FALSE, shift_int=FALSE)
-    }
-    if(return=='image_post_fix'){
-      return(image_post_fix)
-    }
-    #frac_good = length(which(!is.na(image_post_fix))) / prod(dim(image_post_fix))
-    if(is.null(pix_cost_use)){
-      #cost = sum(asinh((image_ref * image_post_fix)/scale/frac_good), na.rm=TRUE)
-      cost = sum(((image_ref - image_post_fix)/scale)^2, na.rm=TRUE)
-    }else{
-      #cost = sum(asinh((image_ref[pix_cost_use] * image_post_fix[pix_cost_use])/scale/frac_good), na.rm=TRUE)
-      cost = sum(((image_ref - image_post_fix[pix_cost_use])/scale)^2, na.rm=TRUE)
-    }
-    #message(par[1],' ',par[2],' ',cost)
-    if(return=='cost'){
-      return(cost)
-    }
-    if(return=='all'){
-      return(list(cost=cost, image_post_fix=image_post_fix))
-    }
-  }
-}
-
 propaneWCSmod = function(input, delta_x = 0, delta_y = 0, delta_rot = 0){
 
   if(inherits(input, 'Rfits_keylist')){
@@ -308,10 +306,10 @@ propaneWCSmod = function(input, delta_x = 0, delta_y = 0, delta_rot = 0){
                                 keyvalues$CD1_2,
                                 keyvalues$CD2_1,
                                 keyvalues$CD2_2
-                                ),
-                              nrow = 2,
-                              byrow = TRUE
-                              )
+    ),
+    nrow = 2,
+    byrow = TRUE
+    )
 
     delta_rot_rad = -delta_rot*pi/180
 
@@ -319,10 +317,10 @@ propaneWCSmod = function(input, delta_x = 0, delta_y = 0, delta_rot = 0){
                             -sin(delta_rot_rad),
                             sin(delta_rot_rad),
                             cos(delta_rot_rad)
-                            ),
-                          nrow = 2,
-                          byrow = TRUE
-                          )
+    ),
+    nrow = 2,
+    byrow = TRUE
+    )
 
     rotmat =  rotmat_keyvalues %*% rotmat_delta
 
@@ -337,11 +335,11 @@ propaneWCSmod = function(input, delta_x = 0, delta_y = 0, delta_rot = 0){
   raw = Rfits_header_to_raw(header)
 
   if(keylist){
-  return(list(keyvalues = keyvalues,
-              header = header,
-              hdr = hdr,
-              raw = raw)
-         )
+    return(list(keyvalues = keyvalues,
+                header = header,
+                hdr = hdr,
+                raw = raw)
+    )
   }else{
     input$keyvalues = keyvalues
     input$header = header
@@ -349,5 +347,79 @@ propaneWCSmod = function(input, delta_x = 0, delta_y = 0, delta_rot = 0){
     input$raw = raw
 
     return(input)
+  }
+}
+
+.map.tran = function(x=0, y=0, delta_x=0, delta_y=0, delta_rot=0, xcen_rot=0, ycen_rot=0){
+  if(delta_rot != 0){
+    x_mod = (x - xcen_rot)*cos(delta_rot*pi/180) + (y - ycen_rot)*sin(delta_rot*pi/180)
+    y_mod = -(x - xcen_rot)*sin(delta_rot*pi/180) + (y - ycen_rot)*cos(delta_rot*pi/180)
+    x_mod = x_mod + xcen_rot + delta_x
+    y_mod = y_mod + ycen_rot + delta_y
+  }else{
+    x_mod = x + delta_x
+    y_mod = y + delta_y
+  }
+  list(x = x_mod, y = y_mod)
+}
+
+.cost_fn = function(par, image_ref, image_pre_fix, scale=1, direction='backward', pix_cost_use=NULL,
+                    shift_int=TRUE, return='cost', WCS_match=TRUE){
+
+  if(shift_int){
+    WCS_match = TRUE
+  }
+
+  if(WCS_match){
+    if(inherits(image_ref, 'Rfits_image')){
+      image_ref = image_ref$imDat
+    }
+
+    if(inherits(image_pre_fix, 'Rfits_image')){
+      image_pre_fix = image_pre_fix$imDat
+    }
+  }
+
+  if(shift_int){
+    cost = .mat_diff_sum(image_ref, image_pre_fix, scale, par[1], par[2])
+    message(par[1],' ',par[2],' ',cost)
+    return(cost)
+  }else{
+    if(WCS_match){
+      if(length(par) == 2){
+        image_post_fix = propaneTran(image_pre_fix, delta_x=par[1], delta_y=par[2], padNA=FALSE, shift_int=FALSE)
+      }else if (length(par)==3){
+        image_post_fix = propaneTran(image_pre_fix, delta_x=par[1], delta_y=par[2], delta_rot=par[3], padNA=FALSE, shift_int=FALSE)
+      }
+    }else{
+      if(length(par) == 2){
+        image_pre_fix = propaneWCSmod(image_pre_fix, delta_x=par[1], delta_y=par[2])
+      }else{
+        image_pre_fix = propaneWCSmod(image_pre_fix, delta_x=par[1], delta_y=par[2], delta_rot=par[3])
+      }
+      image_post_fix = propaneWarp(image_in = image_pre_fix,
+                                   keyvalues_out = image_ref$keyvalues,
+                                   header_out = image_ref$raw,
+                                   direction = 'backward')$imDat
+      image_ref = image_ref$imDat
+    }
+    if(return=='image_post_fix'){
+      return(image_post_fix)
+    }
+    #frac_good = length(which(!is.na(image_post_fix))) / prod(dim(image_post_fix))
+    if(is.null(pix_cost_use)){
+      #cost = sum(asinh((image_ref * image_post_fix)/scale/frac_good), na.rm=TRUE)
+      cost = sum(((image_ref - image_post_fix)/scale)^2, na.rm=TRUE)
+    }else{
+      #cost = sum(asinh((image_ref[pix_cost_use] * image_post_fix[pix_cost_use])/scale/frac_good), na.rm=TRUE)
+      cost = sum(((image_ref - image_post_fix[pix_cost_use])/scale)^2, na.rm=TRUE)
+    }
+    #message(par[1],' ',par[2],' ',cost)
+    if(return=='cost'){
+      return(cost)
+    }
+    if(return=='all'){
+      return(list(cost=cost, image_post_fix=image_post_fix))
+    }
   }
 }
