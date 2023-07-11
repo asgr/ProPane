49e981bcbace4ddec2727c83b0c8c625be2ad4f4
Aaron Robotham <aaron.robotham@uwa.edu.au>
2023-07-11 16:52:57 +0800

    New mask option in propaneMaskPix.

--- R/propaneMaskPix.R
+++ R/propaneMaskPix.R
@@ -1,8 +1,19 @@
-propaneMaskPix = function(image, smooth=1, sigma=10, pixcut=1, cold=FALSE, hot=TRUE, return='image',
+propaneMaskPix = function(image, mask=NULL, smooth=1, sigma=10, pixcut=1, cold=FALSE, hot=TRUE, return='image',
                          patch=FALSE, allow_write=FALSE, plot=FALSE, ...){
   if(!requireNamespace("imager", quietly = TRUE)){
     stop('The imager package is needed for stacking to work. Please install from GitHub asgr/Rfits.', call. = FALSE)
   }
+
+  assert(checkClass(image,'Rfits_image'), checkClass(image,'Rfits_pointer'), checkClass(image,'matrix'))
+  assertNumeric(smooth, len=1)
+  assertNumeric(sigma, len=1)
+  assertIntegerish(pixcut, len=1)
+  assertFlag(cold)
+  assertFlag(hot)
+  assertChoice(return, c('image', 'mask', 'loc'))
+  assertFlag(patch)
+  assertFlag(allow_write)
+  assertFlag(plot)
 
   if(inherits(image, 'Rfits_image')){
     image_data = image$imDat
@@ -13,8 +24,16 @@ propaneMaskPix = function(image, smooth=1, sigma=10, pixcut=1, cold=FALSE, hot=T
       image = image[,]
       image_data = image$imDat
     }
-  }else{
+  }else if(is.matrix(image)){
     image_data = image
+  }else{
+    stop('image input must be Rfits_image, Rfits_pointer of matrix!')
+  }
+
+  if(!is.null(mask)){
+    mask_in_sel = which(mask > 0L)
+    image_orig = image_data
+    image_data[mask_in_sel] = NA
   }
 
   blur = as.matrix(imager::isoblur(imager::as.cimg(image_data),smooth,na.rm=TRUE))
@@ -32,6 +51,11 @@ propaneMaskPix = function(image, smooth=1, sigma=10, pixcut=1, cold=FALSE, hot=T
     thresh_hot = quancuts[2] + (quancuts[2] - quancuts[1])*sigma
   }else{
     thresh_hot = Inf
+  }
+
+  if(!is.null(mask)){
+    image_diff[mask_in_sel] = 0L
+    image_data[mask_in_sel] = image_orig[mask_in_sel]
   }
 
   mask_rough = (image_diff < thresh_cold) | (image_diff > thresh_hot)
@@ -60,12 +84,12 @@ propaneMaskPix = function(image, smooth=1, sigma=10, pixcut=1, cold=FALSE, hot=T
     }
     return(invisible(image))
   }else if(return == 'mask'){
-    mask = matrix(0L, dim(image)[1], dim(image)[2])
-    mask[mask_loc] = 1L
+    mask_out = matrix(0L, dim(image)[1], dim(image)[2])
+    mask_out[mask_loc] = 1L
     if(plot){
       magimage(mask, ...)
     }
-    return(invisible(mask))
+    return(invisible(mask_out))
   }else if(return == 'loc'){
     return(invisible(mask_loc))
   }
