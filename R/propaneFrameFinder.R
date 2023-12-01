3510d8f580caee25812d79433f3ed09c1152b4a1
Aaron Robotham <aaron.robotham@uwa.edu.au>
2023-12-01 17:52:41 +0800

    Added propaneGenWCS to find an optimal WCS based on a list of images to be stacked.

--- R/propaneFrameFinder.R
+++ R/propaneFrameFinder.R
@@ -27,41 +27,6 @@ propaneFrameFinder = function(filelist = NULL,
                            Deccen,
                            FrameInfo[,c('centre_RA','centre_Dec')], rad=rad + FrameInfo$pixscale*sqrt(FrameInfo$dim_1^2 + FrameInfo$dim_2^2)/2/3600, radunit='deg')
 
-  # MatchInfo_Cor_BL = coordmatchsing(RAcen,
-  #                                Deccen,
-  #                                FrameInfo[,c('corner_BL_RA','corner_BL_Dec')], rad=rad, radunit=radunit)
-  #
-  # MatchInfo_Cor_TL = coordmatchsing(RAcen,
-  #                                   Deccen,
-  #                                   FrameInfo[,c('corner_TL_RA','corner_TL_Dec')], rad=rad, radunit=radunit)
-  #
-  # MatchInfo_Cor_TR = coordmatchsing(RAcen,
-  #                                   Deccen,
-  #                                   FrameInfo[,c('corner_TR_RA','corner_TR_Dec')], rad=rad, radunit=radunit)
-  #
-  # MatchInfo_Cor_BR = coordmatchsing(RAcen,
-  #                                   Deccen,
-  #                                   FrameInfo[,c('corner_BR_RA','corner_BR_Dec')], rad=rad, radunit=radunit)
-
-  # Pointers = Rfits_make_list(filelist = filelist,
-  #                            dirlist = dirlist,
-  #                            extlist = extlist,
-  #                            cores = cores)
-
-  # registerDoParallel(cores=cores)
-
-  # point = NULL
-
-  # In_Frame = FrameInfo[,'min_RA'] < RAcen & FrameInfo[,'max_RA'] > RAcen &
-  #            FrameInfo[,'min_Dec'] < Deccen & FrameInfo[,'max_Dec'] > Deccen
-
-  # GoodID = c(MatchInfo_Cen$ID,
-  #            MatchInfo_Cor_BL$ID,
-  #            MatchInfo_Cor_TL$ID,
-  #            MatchInfo_Cor_TR$ID,
-  #            MatchInfo_Cor_BR$ID,
-  #            which(In_Frame)
-  #            )
 
   GoodID = MatchInfo_Cen$ID
 
@@ -77,7 +42,6 @@ propaneFrameFinder = function(filelist = NULL,
   }
 
   FrameInfo = FrameInfo[GoodID,]
-  #FrameInfo = cbind(FrameInfo, sep = MatchInfo$sep)
 
   longlim = RAcen + c(-rad,rad)/cos(Deccen*pi/180)
   latlim = Deccen + c(-rad,rad)
@@ -129,4 +93,95 @@ propaneFrameFinder = function(filelist = NULL,
   legend('topleft', legend = paste('Nmatch:', Nmatch), bg=bg)
 
   return(invisible(FrameInfo))
+}
+
+propaneGenWCS = function(image_list = NULL, filelist = NULL, dirlist = NULL, CRVAL1 = NULL,
+                          CRVAL2 = NULL, pixscale = NULL, NAXIS1 = NULL, NAXIS2 = NULL,
+                          CRPIX1 = NULL, CRPIX2 = NULL, CTYPE1 = "RA---TAN", CTYPE2 = "DEC--TAN",
+                          CUNIT1 = "deg", CUNIT2 = "deg", ...){
+  if(!is.null(image_list)){
+    image = NULL
+    info = foreach(image = image_list, .combine='rbind')%do%{
+      current_info = list()
+
+      temp_cen = centre(image, ...)
+      if(is.na(temp_cen[1])){
+        temp_cen = rep(NA, 2)
+      }
+      current_info = c(current_info,
+                       centre_RA = temp_cen[1], centre_Dec = temp_cen[2]
+      )
+
+      temp_cor = corners(image, ...)
+      if(is.na(temp_cor[1])){
+        temp_cor = matrix(NA, 4, 2)
+      }
+      current_info = c(current_info,
+                       corner_BL_RA = temp_cor[1,1], corner_BL_Dec = temp_cor[1,2],
+                       corner_TL_RA = temp_cor[2,1], corner_TL_Dec = temp_cor[2,2],
+                       corner_TR_RA = temp_cor[3,1], corner_TR_Dec = temp_cor[3,2],
+                       corner_BR_RA = temp_cor[4,1], corner_BR_Dec = temp_cor[4,2]
+      )
+
+      temp_ext = extremes(image, ...)
+      if(is.na(temp_ext[1])){
+        temp_ext = matrix(NA, 3, 2)
+      }
+      current_info = c(current_info,
+                       min_RA = temp_ext[1,1], min_Dec = temp_ext[1,2],
+                       max_RA = temp_ext[2,1], max_Dec = temp_ext[2,2],
+                       range_RA = temp_ext[3,1], range_Dec = temp_ext[3,2]
+      )
+
+      temp_pixscale = pixscale(image, ...)
+      current_info = c(current_info, pixscale = temp_pixscale)
+
+      return(as.data.frame(current_info))
+    }
+  }else{
+    info = Rfits_key_scan(filelist=filelist, dirlist=dirlist,
+                                get_centre = TRUE,
+                                get_corners = TRUE,
+                                get_extremes = TRUE,
+                                get_pixscale = TRUE,
+                                ...)
+  }
+
+  if(is.null(CRVAL1)){
+    CRVAL1 = (min(info$min_RA) + max(info$max_RA))/2
+  }
+  if(is.null(CRVAL2)){
+    CRVAL2 = (min(info$min_Dec) + max(info$max_Dec))/2
+  }
+  if(is.null(pixscale)){
+    pixscale = min(info$pixscale)
+  }
+  if(is.null(NAXIS1)){
+    NAXIS1 = ceiling(3600*diff(range(info$min_RA, info$max_RA)) * cos(max(abs(info$min_Dec), abs(info$max_Dec))*pi/180)/pixscale)
+  }
+  if(is.null(NAXIS2)){
+    NAXIS2 = ceiling(3600*diff(range(info$min_Dec, info$max_Dec))/pixscale)
+  }
+  if(is.null(CRPIX1)){
+    NAXIS1/2 + 0.5
+  }
+  if(is.null(CRPIX2)){
+    NAXIS2/2 + 0.5
+  }
+
+  keyvalues = Rwcs_setkeyvalues(
+    CRVAL1 = CRVAL1,
+    CRVAL2 = CRVAL2,
+    pixscale = pixscale,
+    NAXIS1 = NAXIS1,
+    NAXIS2 = NAXIS2,
+    CRPIX1 = CRPIX1,
+    CRPIX2 = CRPIX2,
+    CTYPE1 = CTYPE1,
+    CTYPE2 = CTYPE2,
+    CUNIT1 = CUNIT1,
+    CUNIT2 = CUNIT2
+  )
+
+  return(keyvalues)
 }
